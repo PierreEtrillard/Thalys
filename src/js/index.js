4e81fc4339357db3777cf6342baddbfe0b343a44
Pierre ETRILLARD <pierre.etrillard@live.fr>
2024-07-25 18:23:27 +0200

    creation du squelette du header et de l'eventListener au click du bouton menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-alert("script run")
+isMenuOpened = false;
+menuBtn = document.getElementById("menuTrigger");
+menuBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  toggleMenu();
+});
+function toggleMenu() {
+  isMenuOpened ? (isMenuOpened = false) : (isMenuOpened = true);
+  alert("isMenuOpened ?: " + isMenuOpened);
+}
