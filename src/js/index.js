face7ea64ab24568fe5540ffb51798a2d9cb1b91
Pierre ETRILLARD <pierre.etrillard@live.fr>
2024-07-26 12:15:58 +0200

    implementation du menu toggler

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,16 @@
 isMenuOpened = false;
 menuBtn = document.getElementById("menuTrigger");
+menu = document.querySelector('.menu')
+menuStripes = document.querySelectorAll('.stripe')
+
 menuBtn.addEventListener("click", (event) => {
   event.preventDefault();
   toggleMenu();
 });
 function toggleMenu() {
-  isMenuOpened ? (isMenuOpened = false) : (isMenuOpened = true);
-  alert("isMenuOpened ?: " + isMenuOpened);
+  menu.classList.toggle('menu-hidden') 
+  menuStripes[0].classList.toggle('stripe-top-opened')
+  menuStripes[1].classList.toggle('stripe-middle-opened')
+  menuStripes[2].classList.toggle('stripe-bottom-opened')
+
 }
