6c2f81d1dde77ca6352ea427082007c67bcf3f70
Pierre ETRILLARD <pierre.etrillard@live.fr>
2024-07-30 18:13:39 +0200

    ajout du bouton scrollUp

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Thalys
 Site vitrine de Thalys développement 
+## Maintenance 
+Sur demande du client aucun framework n'est utilisé pour le développement du projet et celui-ci ne dépend d'aucune librairie tiers pour la phase de développement ormis sass.
+Les dépendances suivante sont toutefois necessaires pour la mise en production:
+    autoprefixer
+    cssnano
+    html-minifier-terser
+    postcss
+    postcss-cli
+L'ensemble de ces dépendance ainsi que sass est installable en une seul commande à executer dans le dossier 'Thalys':
+npm install --save
+node est facilement installable sur tous les OS en suivant ce lien[https://nodejs.org/fr/download/] et est necessaire pour utiliser npm (node package manager)
+## Deploiement
+une seul commande est nécesaire à la production: 'npm run build' celle ci générera des fichier minifiés des fichiers html,css et js au sein du dossier dist.
+Pour la mise en production il suffit d'envoyé les repertoire dist, asset, fonts et les fichiers favicon et license sur le serveur.
+## CSS
+la fichier main.scss centralise toutes les régles et dépend des partials annotées d'un underscore dans le même fichier. main.scss est structué ainsi : les variables global en premier puis les régles sont écrite dans le déroulé du index.html. les régles spécifiques au pages aboout, contact et offre sont en queue de fichier dans l'ordre précité. 
--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,44 @@
+// Déclarations des variables
 let isMenuOpened = false;
-const menuBtn = document.getElementById("menuTrigger");
+const scrollUpBtn = document.getElementById("scrollUp-btn");
 const menu = document.querySelector(".menu");
 const menuStripes = document.querySelectorAll(".stripe");
+const feelingsTitles = document.querySelectorAll(".feelings");
 const comments = document.querySelectorAll(".comment");
 let currentCommentIndex = 0;
 const commentSelector = document.getElementById("comment-selector");
-const commentSelectorBtns = document.querySelectorAll(
-  '#comment-selector input[name="commment-selection"]'
-);
+const commentSelectorBtns = document.querySelectorAll('#comment-selector input[name="commment-selection"]');
 const previewComment = document.querySelector(".arrow-crsl-left");
 const nextComment = document.querySelector(".arrow-crsl-right");
 
+let hideScrollUpBtnTimeout;
+
 //--------------------------INITIALISATION--------------------------------//
-menuBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  toggleMenu();
+
+// Ajout d'un écouteur d'événement pour le scroll
+window.addEventListener("scroll", () => scrollUpBtnAppeared(0.2));
+
+// Initialisation du DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+  feelingsTitles.forEach((element) => {
+    viewPortEntranceObserver.observe(element);
+  });
 });
 
+// Gestion des événements pour les boutons de sélection de commentaire
 commentSelector.addEventListener("change", (event) => {
   event.preventDefault();
   currentCommentIndex = parseInt(event.target.value, 10);
   switchComment(currentCommentIndex);
 });
 
+// Gestion des clics sur les boutons de navigation des commentaires
 previewComment.addEventListener("click", () => {
   if (currentCommentIndex > 0) {
     currentCommentIndex -= 1;
     switchComment(currentCommentIndex);
   }
 });
-
 nextComment.addEventListener("click", () => {
   if (currentCommentIndex < comments.length - 1) {
     currentCommentIndex += 1;
@@ -37,10 +46,69 @@ nextComment.addEventListener("click", () => {
   }
 });
 
+// Initialisation des états du commutateur de commentaire
 commentSwitcherState();
-commentDisplayer(6); // joue le carrousel toutes les n seconde
+
+// Démarrage du carrousel de commentaires
+commentDisplayer(6); // Affiche le carrousel toutes les n secondes
+
 //----------------------------FONCTIONS------------------------------//
-// animation de l'icone menu (version mobile exclusivement)
+
+// Fonction pour afficher le bouton de retour en haut de la page
+function scrollUpBtnAppeared(minScrollHeight) {
+  const scrollYposition = window.scrollY;
+  const viewPortHeight = window.innerHeight;
+  const triggerMinHeight = viewPortHeight * minScrollHeight; // Début du déclenchement à n% de scrollY
+
+  if (scrollYposition >= triggerMinHeight) {
+    scrollUpBtn.classList.remove("hidden");
+
+    // Annuler tout délai précédent
+    if (hideScrollUpBtnTimeout) {
+      clearTimeout(hideScrollUpBtnTimeout);
+    }
+
+    // Planifier la suppression de la classe après 2 secondes
+    hideScrollUpBtnTimeout = setTimeout(() => {
+      scrollUpBtn.classList.add("hidden");
+    }, 2000);
+  } else {
+    // Si le défilement est en dessous du seuil, masquer immédiatement
+    scrollUpBtn.classList.add("hidden");
+
+    // Annuler tout délai précédent
+    if (hideScrollUpBtnTimeout) {
+      clearTimeout(hideScrollUpBtnTimeout);
+    }
+  }
+}
+
+// Fonction pour faire défiler la page vers le haut
+function scrollUp() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+// Observeur d'entrée des éléments dans le viewport
+const observerOptions = {
+  threshold: 0.1, // Le seuil de 0.1 signifie que l'observer déclenchera une entrée lorsque 10% de l'élément sera visible dans le viewport.
+};
+
+const viewPortEntranceObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("appeared");
+        observer.unobserve(entry.target); // Désabonne pour éviter les fuites de mémoire
+      }
+    });
+  },
+  observerOptions
+);
+
+// Fonction pour basculer l'état du menu
 function toggleMenu() {
   menu.classList.toggle("menu-hidden");
   menuStripes[0].classList.toggle("stripe-top-opened");
@@ -48,14 +116,14 @@ function toggleMenu() {
   menuStripes[2].classList.toggle("stripe-bottom-opened");
 }
 
-// grise les fléches du carrousel
+// Fonction pour gérer l'état des flèches du carrousel
 function commentSwitcherState() {
   if (currentCommentIndex <= 0) {
     previewComment.style.filter = "grayscale(1)";
   } else {
     previewComment.style.filter = "";
   }
-  
+
   if (currentCommentIndex >= comments.length - 1) {
     nextComment.style.filter = "grayscale(1)";
   } else {
@@ -63,26 +131,27 @@ function commentSwitcherState() {
   }
 }
 
-// ajout/suppression de la class 'comment-selected' (affiche le commentaire)
+// Fonction pour changer de commentaire
 function switchComment(index) {
   commentSwitcherState();
-  commentSelectorBtns[index].checked = true;// check le bouton (utile dans l'utilisation des fléches de navigation du carrousel)
+  commentSelectorBtns[index].checked = true; // Check le bouton (utile dans l'utilisation des flèches de navigation du carrousel)
   comments.forEach((comment) => {
-    comment.classList.remove("comment-selected");// nettoie les comments (evite les doublons)
+    comment.classList.remove("comment-selected"); // Nettoie les commentaires (évite les doublons)
   });
   comments[index].classList.add("comment-selected");
 }
 
+// Fonction pour faire défiler les commentaires automatiquement
 async function commentDisplayer(delay){
-setInterval(() => {
-  (currentCommentIndex < comments.length -1)?
-   currentCommentIndex += 1 :
-   currentCommentIndex = 0;
-  console.log('switch comment to ' + currentCommentIndex);
-  switchComment(currentCommentIndex)
-}, delay * 1000);
-}
-// envoi du formulaire de contact
+  setInterval(() => {
+    (currentCommentIndex < comments.length -1)?
+     currentCommentIndex += 1 :
+     currentCommentIndex = 0;
+    switchComment(currentCommentIndex)
+  }, delay * 1000);
+  }
+
+// Fonction de démonstration pour l'envoi du formulaire de contact (ajoute une action réelle ici)
 function postToIframe(event) {
-  alert("envoi de " + event.target);
+  alert("Envoi de " + event.target);
 }
