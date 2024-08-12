be95ba0acc7129a15cec00fa5fece4318fff274c
Pierre ETRILLARD <pierre.etrillard@live.fr>
2024-08-12 11:33:54 +0200

    achévement du service 'carroussel' et connection du service aux carrousels des pages accueil et offre

--- src/js/accueil.js
+++ src/js/accueil.js
@@ -1,19 +1,23 @@
+import { commentSwitcher, selectedComment } from "./carousel.service.js";
 import sendMessage from "./form.service.js";
-// Déclarations des variables
+
+//-------------------------DECLARATIONS DES VARIABLES--------------------------------//
 const feelingsTitles = document.querySelectorAll(".feelings");
 const targetsTitles = document.querySelectorAll(".targets");
-const comments = document.querySelectorAll(".comment");
-let currentCommentIndex = 0;
-const commentSelector = document.getElementById("comment-selector");
-const commentSelectorBtns = document.querySelectorAll(
-  '#comment-selector input[name="commment-selection"]'
-);
-const previewComment = document.querySelector(".arrow-crsl-left");
-const nextComment = document.querySelector(".arrow-crsl-right");
-const mailSender = document.getElementById('mail-sender')
-//--------------------------INITIALISATION--------------------------------//
-/* DOMContentLoaded garanti le chargement complet du DOM avant le declenchement des listener 
-evite ainsi d'écouter des elements vide*/
+
+// Carrousel:
+const carrousel = document.getElementById("accueil-carousel");
+const commentSelector = carrousel.querySelector(".comment-selector");
+const previewComment = carrousel.querySelector(".arrow-crsl-left");
+const nextComment = carrousel.querySelector(".arrow-crsl-right");
+let commentSelected = 0;
+const delay = 8; // Temps en seconde entre chaque affichage de commentaire
+let commentDisplayer; // Cible l'afficheur pour le démarrer ou le réinitialiser
+
+// Formulaire de contact
+const mailSender = document.getElementById("mail-sender");
+
+//--------------------------INITIALISATION DES LISTENERS--------------------------------//
 document.addEventListener("DOMContentLoaded", () => {
   feelingsTitles.forEach((element) => {
     viewPortEntranceObserver.observe(element);
@@ -22,83 +26,62 @@ document.addEventListener("DOMContentLoaded", () => {
     viewPortEntranceObserver.observe(element);
   });
 
-// Gestion des événements pour les boutons de sélection de commentaire
-commentSelector.addEventListener("change", (event) => {
-  event.preventDefault();
-  currentCommentIndex = parseInt(event.target.value, 10);
-  switchComment(currentCommentIndex);
+  // Gestion des événements pour les boutons de sélection de commentaire
+  commentSelector.addEventListener("change", (event) => {
+    event.preventDefault();
+    commentSwitcher(event.target.value - commentSelected, carrousel, commentSelected);
+    commentSelected = selectedComment;
+    commentRunner();
+  });
+
+  // Gestion des clics sur les boutons de navigation des commentaires
+  previewComment.addEventListener("click", () => {
+    commentSwitcher(-1, carrousel, commentSelected);
+    commentSelected = selectedComment;
+    commentRunner();
+  });
+
+  nextComment.addEventListener("click", () => {
+    commentSwitcher(+1, carrousel, commentSelected);
+    commentSelected = selectedComment;
+    commentRunner();
+  });
+
+  // Démarrage du carrousel de commentaires
+  commentRunner(); // Démarre l'afficheur de commentaires dès le chargement
+  // Envoi du formulaire de contact au back-end
+  mailSender.addEventListener("submit", (event) => {
+    postToMailer(event);
+  });
 });
 
-// Gestion des clics sur les boutons de navigation des commentaires
-previewComment.addEventListener("click", () => {
-  if (currentCommentIndex > 0) {
-    currentCommentIndex -= 1;
-    switchComment(currentCommentIndex);
-  }
-});
-nextComment.addEventListener("click", () => {
-  if (currentCommentIndex < comments.length - 1) {
-    currentCommentIndex += 1;
-    switchComment(currentCommentIndex);
-  }
-});
-// Initialisation des états du commutateur de commentaire
-commentSwitcherState();
-
-// Démarrage du carrousel de commentaires
-commentDisplayer(8); // Affiche un commentaire dans le carrousel toutes les n secondes
-
-// Envoi du formulaire de contact au back-end
-mailSender.addEventListener('submit', (event)=>{postToMailer(event)})
-});
 //----------------------------FONCTIONS------------------------------//
+
 // Observeur d'entrée des éléments dans le viewport
 const observerOptions = {
-  threshold: 1, // Le seuil de 1.4 signifie que l'observer déclenchera une entrée lorsque 100% de l'élément sera visible dans le viewport.
+  threshold: 1, // Le seuil de 1 signifie que l'observer déclenchera une entrée lorsque 100% de l'élément sera visible dans le viewport.
 };
-const viewPortEntranceObserver = new IntersectionObserver(
-  (entries, observer) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.remove(...["hidden", "hidden-smooth"]);
-        observer.unobserve(entry.target); // Désabonne pour éviter les fuites de mémoire
-      }
-    });
-  },
-  observerOptions
-);
-// Fonction pour gérer l'état des flèches du carrousel
-function commentSwitcherState() {
-  if (currentCommentIndex <= 0) {
-    previewComment.style.filter = "grayscale(1)";
-  } else {
-    previewComment.style.filter = "";
-  }
 
-  if (currentCommentIndex >= comments.length - 1) {
-    nextComment.style.filter = "grayscale(1)";
-  } else {
-    nextComment.style.filter = "";
-  }
-}
-
-// Fonction pour changer de commentaire
-function switchComment(index) {
-  commentSwitcherState();
-  commentSelectorBtns[index].checked = true; // Check le bouton (utile dans l'utilisation des flèches de navigation du carrousel)
-  comments.forEach((comment) => {
-    comment.classList.remove("comment-selected"); // Nettoie les commentaires (évite les doublons)
+const viewPortEntranceObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove(...["hidden", "hidden-smooth"]);
+      observer.unobserve(entry.target); // Désabonne pour éviter les fuites de mémoire
+    }
   });
-  comments[index].classList.add("comment-selected");
-}
+}, observerOptions);
 
 // Fonction pour faire défiler les commentaires automatiquement
-async function commentDisplayer(delay) {
-  setInterval(() => {
-    currentCommentIndex < comments.length - 1
-      ? (currentCommentIndex += 1)
-      : (currentCommentIndex = 0);
-    switchComment(currentCommentIndex);
+function commentRunner() {
+  // Remet le compteur de l'afficheur à zéro s'il est lancé
+  if (commentDisplayer) {
+    clearInterval(commentDisplayer);
+  }
+
+  // Démarre un nouvel intervalle
+  commentDisplayer = setInterval(() => {
+    commentSwitcher(+1, carrousel, commentSelected);
+    commentSelected = selectedComment;
   }, delay * 1000);
 }
 
--- src/js/pages.js
+++ src/js/pages.js
@@ -1,31 +1,79 @@
 import sendMessage from "./form.service.js";
-import commentSwitcher from "./carousel.service.js";
+import { commentSwitcher, selectedComment } from "./carousel.service.js";
 // Déclarations des variables
 let selectedPage = window.location.search.slice(1); // slice(1) extrait tout la string après le premier caractère (débarrase le ? du parmas)
 const menulinks = document.querySelectorAll("nav.menu>ul>li>a");
 const mainContenair = document.getElementById("content-eager");
-let CommentIndex = 0;
+let comment1Selected = 0;
+let comment2Selected = 0;
 //--------------------------INITIALISATION--------------------------------//
 // Initialisation du DOMContentLoaded avant l'injection du main
 document.addEventListener("DOMContentLoaded", async () => {
   switch (selectedPage) {
     case "offre":
-      await injectHTML("/pages/offre.html").then(()=>{
-        const comments = document.querySelectorAll(".comment");
-        const commentSelector = document.getElementById("comment-selector");
-        const commentSelectorBtns = document.querySelectorAll(
-          '#comment-selector input[name="commment-selection"]'
-        );
-        const previewComment = document.querySelector(".arrow-crsl-left");
-        const nextComment = document.querySelector(".arrow-crsl-right");
+      //          ***INJECTION DE LA PAGE OFFRE***
+      await injectHTML("/pages/offre.html").then(() => {
         menulinks[1].classList.toggle("link-selected");
+        //carrousel 1
+        const shortSupportComments = document.getElementById("short-support");
+        const previewComment1 =
+          shortSupportComments.querySelector(".arrow-crsl-left");
+        const nextComment1 =
+          shortSupportComments.querySelector(".arrow-crsl-right");
+        const selectorBtn1 =
+          shortSupportComments.querySelector(".comment-selector");
+        previewComment1.addEventListener("click", () => {
+          commentSwitcher(-1, shortSupportComments, comment1Selected);
+          comment1Selected = selectedComment;
+        });
+        nextComment1.addEventListener("click", () => {
+          commentSwitcher(+1, shortSupportComments, comment1Selected);
+          comment1Selected = selectedComment;
+        });
+        selectorBtn1.addEventListener("change", (event) => {
+          event.preventDefault();
+          commentSwitcher(
+            event.target.value - comment1Selected,
+            shortSupportComments,
+            comment1Selected
+          );
+          comment1Selected = selectedComment;
+        });
+        //carrousel 2
+        const individualSupportComments =
+          document.getElementById("individual-support");
+        const previewComment2 =
+          individualSupportComments.querySelector(".arrow-crsl-left");
+        const nextComment2 =
+          individualSupportComments.querySelector(".arrow-crsl-right");
+        const selectorBtn2 =
+          individualSupportComments.querySelector(".comment-selector");
+        previewComment2.addEventListener("click", () => {
+          commentSwitcher(-1, individualSupportComments, comment2Selected);
+          comment2Selected = selectedComment;
+        });
+        nextComment2.addEventListener("click", () => {
+          commentSwitcher(+1, individualSupportComments, comment2Selected);
+          comment2Selected = selectedComment;
+        });
+        selectorBtn2.addEventListener("change", (event) => {
+          event.preventDefault();
+          commentSwitcher(
+            event.target.value - comment2Selected,
+            individualSupportComments,
+            comment2Selected
+          );
+          comment2Selected = selectedComment;
+        });
       });
       break;
     case "about":
+      //          ***INJECTION DE LA PAGE PROPOS***
       injectHTML("/pages/about.html");
       menulinks[2].classList.toggle("link-selected");
       break;
     case "contact":
+      //          ***INJECTION DE LA PAGE CONTACT***
       await injectHTML("/pages/contact.html").then(() => {
         const contactForm = document.getElementById("contact-form");
         contactForm.addEventListener("submit", (event) => formSubmit(event));
@@ -33,9 +81,11 @@ document.addEventListener("DOMContentLoaded", async () => {
       menulinks[3].classList.toggle("link-selected");
       break;
     case "legal":
+      //          ***INJECTION DE LA PAGE MENTIONS LEGAL***
       injectHTML("/pages/legal.html");
       break;
     case "confidentiality":
+      //          ***INJECTION DE LA PAGE DECLARATION DE CONFIDENTIALITEE***
       injectHTML("/pages/confidentiality.html");
       break;
     default:
@@ -49,8 +99,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 function formSubmit(event) {
   event.preventDefault();
   const message = document.getElementById("message").value;
-  const clientEmail = document.getElementById("client-email").value
-sendMessage(message, clientEmail);
+  const clientEmail = document.getElementById("client-email").value;
+  sendMessage(message, clientEmail);
 }
 function injectHTML(htmlFileUrl) {
   return fetch(htmlFileUrl)
